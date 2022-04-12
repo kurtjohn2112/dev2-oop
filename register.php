bf15bd00226a8cb6296c68e5a83165a79d9b1926
kurt john <kurtjohn2112>
2022-04-12 11:08:08 +0800

    added form:post

--- register.php
+++ register.php
@@ -11,6 +11,11 @@
 
   </head>
   <body>
+
+  <form action="" method="post">
+
+  <input type="text" name="" id="" class="form-control">
+  </form>
       
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
